905c484a776248ebd82e0593ce0ff7477469948b
caliipay <calimadz71@gmail.com>
2017-11-05 16:05:48 +0800

    CHanged maps padding-left

--- includes/morris-data.php
+++ includes/morris-data.php
@@ -185,20 +185,28 @@ else
 
 <script type="text/javascript">
   $(function() {
-Morris.Bar({
-  element: 'morris-donut-religion',
-  data: [
-    { y: '2006', a: 100, b: 90 },
-    { y: '2007', a: 75,  b: 65 },
-    { y: '2008', a: 50,  b: 40 },
-    { y: '2009', a: 75,  b: 65 },
-    { y: '2010', a: 50,  b: 40 },
-    { y: '2011', a: 75,  b: 65 },
-    { y: '2012', a: 100, b: 90 }
-  ],
-  xkey: 'y',
-  ykeys: ['a', 'b'],
-  labels: ['Series A', 'Series B']
-});
+ Morris.Donut({
+        element: 'morris-donut-religion',
+        data: [
+        <?php  foreach($religion_data as $row) {
+             $religion = $row['Religion'];
+             if($religion == NULL)
+             {
+              $religion = "Unspecified";
+             }
+             $idp_count = $row['TOTAL'];
+             
+        ?>
+        {
+            label: '<?php echo $religion; ?>',
+            value: <?php echo $idp_count; ?>
+        },
+
+        <?php } ?>
+        ],
+        resize: true
+
+      
+    });
    });
 </script>
